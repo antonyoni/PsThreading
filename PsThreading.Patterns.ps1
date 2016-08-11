c689410ee8f9ec4914d9c43c5a9f390d3599af7d
antonyoni <a@arbo.red>
2016-08-11 22:08:22 +0100

    Reworking for ease of use

--- PsThreading.Patterns.ps1
+++ PsThreading.Patterns.ps1
@@ -9,22 +9,39 @@
 ################################################################################
 
 $PsThreading = [pscustomobject]@{
-    DataStructure = [pscustomobject]@{
-        Dictionary = [System.Collections.Concurrent.ConcurrentDictionary`2[object,object]]
-        Queue      = [System.Collections.Concurrent.ConcurrentQueue[object]]
-        Stack      = [System.Collections.Concurrent.ConcurrentStack[object]]
-        Bag        = [System.Collections.Concurrent.ConcurrentBag[object]]
-    }
-    Pattern = [pscustomobject]@{
-        WorkerOnly           = $null
-        ProducerConsumer     = $null
-        ProducerWorkerWriter = $null
-    }
+    Parameter = [pscustomobject]@{
+        WorkerOnly = @{
+            WorkQueue = New-Object System.Collections.Concurrent.ConcurrentQueue[object]
+        }
+        ProducerConsumer = @{
+            WorkQueue = New-Object System.Collections.Concurrent.ConcurrentQueue[object]
+            Settings  = (& {
+                $sets = New-Object 'System.Collections.Concurrent.ConcurrentDictionary`2[string,object]'
+                $sets['NumberToProduce'] = 10000
+                $sets['ProducerIsDone']  = $false
+                return $sets
+            })
+            ResultSet = New-Object System.Collections.Concurrent.ConcurrentBag[object]
+        } 
+        ProducerWorkerWriter = @{
+            WorkQueue = New-Object System.Collections.Concurrent.ConcurrentQueue[object]
+            Settings  = (& {
+                $sets = New-Object 'System.Collections.Concurrent.ConcurrentDictionary`2[string,object]'
+                $sets['OutPath']         = (Join-Path (Get-Location) out.txt)
+                $sets['NumberToProduce'] = 10000
+                $sets['SleepTime']       = 10
+                $sets['ProducerIsDone']  = $false
+                $sets['WorkersAreDone']  = $false
+                return $sets
+            })
+            ResultSet = New-Object System.Collections.Concurrent.ConcurrentBag[object]
+        }
+    } 
     Thread  = [pscustomobject]@{
         Consumer = {
             Param($ThreadId, $WorkQueue, $Settings, $ResultSet)
-            $item = New-Object psobject
-            while (!$Settings['IsDone'] -or $WorkQueue.Count -gt 0) {
+            $item = ""
+            while (!$Settings['ProducerIsDone'] -or $WorkQueue.Count -gt 0) {
                 if ($WorkQueue.TryDequeue([ref]$item)) {
                     #process item
                     $ResultSet.Add("Thread $ThreadId has processed '$item'")
@@ -35,33 +52,39 @@ $PsThreading = [pscustomobject]@{
         }
         Producer = {
             Param($ThreadId, $WorkQueue, $Settings)
-            1..$Settings['Produce'] | % {
+            1..$Settings['NumberToProduce'] | % {
                 $WorkQueue.Enqueue("item number $_")
             }
-            $Settings['IsDone'] = $true
+            $Settings['ProducerIsDone'] = $true
         }
         Worker = {
             Param($ThreadId, $WorkQueue)
             $item = ""
             while ($WorkQueue.TryDequeue([ref]$item)) {
-                # process item here
+                # process item
                 Write-Output "$ThreadId -> $item"
             }
         }
         Writer = {
             Param($ThreadId, $Settings, $ResultSet)
             $item = ""
-            while (!$Settings['IsDone'] -or $ResultSet.Count -gt 0) {
+            $outFile = $Settings['OutputPath']
+            $sleepTime = $Settings['SleepTime']
+            "$(Get-Date -Format "yyyyMMddHHmmss") -> Writer starting" | Out-File -FilePath $outFile
+            while (!$Settings['WorkersAreDone'] -or $ResultSet.Count -gt 0) {
                 if ($ResultSet.TryTake([ref]$item)) {
-                    Write-Host $item
+                    $item | Out-File -FilePath $outFile -Append
                 } else {
-                    Start-Sleep -Milliseconds $Settings['SleepTime']
+                    Start-Sleep -Milliseconds $sleepTime
                 }
             }
+            "$(Get-Date -Format "yyyyMMddHHmmss") -> Writer id done."
         }
     }
     Utility = [pscustomobject]@{}
 }
+
+################################################################################
 
 $numCores = {
     return Get-WmiObject Win32_Processor |
@@ -84,74 +107,6 @@ Add-Member -InputObject $PsThreading.Utility `
            -MemberType ScriptProperty `
            -Name LogicalCpus `
            -Value $numCPUs
-
-################################################################################
-
-$PsThreading.Pattern.WorkerOnly = [pscustomobject]@{
-    Thread = (New-Thread -ScriptBlock $PsThreading.Thread.Worker `
-                         -Number $PsThreading.Utility.LogicalCpus)
-    Parameters = @{
-        WorkQueue = & {
-             $q = New-Object $PsThreading.DataStructure.Queue
-             1..1000 | % { $q.Enqueue("item $_") }
-             return $q
-        }
-    }
-    MaxThreads = $PsThreading.Utility.LogicalCpus
-}
-
-################################################################################
-
-$PsThreading.Pattern.ProducerConsumer = [pscustomobject]@{
-    Thread = @(
-        New-Thread -ScriptBlock $PsThreading.Thread.Producer `
-                   -Type "Producer" `
-                   -Weight 100
-        New-Thread -ScriptBlock $PsThreading.Thread.Consumer `
-                   -Type "Consumer" `
-                   -Number ($PsThreading.Utility.LogicalCpus - 1)
-    )
-    Parameters = @{
-        Settings  = & {
-            $s= New-Object $PsThreading.DataStructure.Dictionary
-            $s['IsDone']    = $false
-            $s['Produce']   = 10000
-            $s['SleepTime'] = 10
-            return $s
-        }
-        WorkQueue = New-Object $PsThreading.DataStructure.Queue
-        ResultSet = New-Object $PsThreading.DataStructure.Bag
-    }
-    MaxThreads = $PsThreading.Utility.LogicalCpus
-}
-
-################################################################################
-
-$PsThreading.Pattern.ProducerWorkerWriter = [pscustomobject]@{
-    Thread = @(
-        New-Thread -ScriptBlock $PsThreading.Thread.Producer `
-                   -Type "Producer" `
-                   -Weight 100
-        New-Thread -ScriptBlock $PsThreading.Thread.Consumer `
-                   -Type "Worker" `
-                   -Number ($PsThreading.Utility.LogicalCpus - 1) `
-                   -Weight 10
-        New-Thread -ScriptBlock $PsThreading.Thread.Writer `
-                   -Type "Writer"
-    )
-    Parameters = @{
-        Settings  = & {
-            $s= New-Object $PsThreading.DataStructure.Dictionary
-            $s['IsDone']    = $false
-            $s['Produce']   = 10000
-            $s['SleepTime'] = 10
-            return $s
-        }
-        WorkQueue = New-Object $PsThreading.DataStructure.Queue
-        ResultSet = New-Object $PsThreading.DataStructure.Bag
-    }
-    MaxThreads = $PsThreading.Utility.LogicalCpus
-}
 
 ################################################################################
 
